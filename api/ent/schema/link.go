48e6e3438fe5ddd976d8e0381e3d957fb642bf33
ippsav <mehdiboujid@gmail.com>
2022-02-01 07:11:59 +0000

    setup schema

---
+++ api/ent/schema/link.go
@@ -0,0 +1,35 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+// Link holds the schema definition for the Link entity.
+type Link struct {
+	ent.Schema
+}
+
+// Fields of the Link.
+func (Link) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).Default(uuid.New),
+		field.String("title").NotEmpty(),
+		field.String("description").NotEmpty(),
+		field.String("image_url").NotEmpty(),
+		field.String("url").NotEmpty(),
+		field.Time("created_at").Default(time.Now),
+		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now),
+	}
+}
+
+// Edges of the Link.
+func (Link) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("owner", User.Type).Ref("bookmarks").Unique(),
+	}
+}
---
+++ api/ent/schema/user.go
@@ -0,0 +1,35 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).Default(uuid.New),
+		field.String("email").Unique().NotEmpty(),
+		field.String("password").NotEmpty(),
+		field.String("image_url"),
+		field.Enum("role").Values("ADMIN", "USER").Default("ADMIN"),
+		field.Time("created_at").Default(time.Now),
+		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("bookmarks", Link.Type),
+	}
+}
